3a28e3110e5af5f7756d1ad2201be2d0621e2e28
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-12-22 20:46:36 -0500

    updated App component to include static weather method to add weather to state

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,6 @@ Weather.fromOpenWeatherMapResponse = function(openWeatherMapResponse) {
   return new Weather(location, temperature, description);
 }
 
-
 class App extends Component {
 
 
@@ -31,9 +30,7 @@ class App extends Component {
 
       this.state = {
         data: [],
-        // city: '' ,
         zipCode: '',
-        // weather: [],
         weather: {
           location: '',
           temp: null,
@@ -52,9 +49,8 @@ class App extends Component {
 
 
     handleInput = (e) => {
-      // if (e.which === 13) {
-          e.preventDefault();
-    let input = e.target.value;
+      e.preventDefault();
+      let input = e.target.value;
 
       this.setState({ zipCode: input })
       console.log("state zip ", this.state.zipCode);
@@ -62,7 +58,7 @@ class App extends Component {
 
 
 handleSubmit = (e) => {
-      e.preventDefault();
+   e.preventDefault();
    fetch(`https://api.openweathermap.org/data/2.5/forecast?zip=${this.state.zipCode}&&APPID=725e07c224d8f9013d5380b8c4954377`)
         .then(response => response.json())
         // .then(weatherResponse =>  console.log(weatherResponse))
@@ -73,18 +69,9 @@ handleSubmit = (e) => {
             _ => console.log("this.state.weather: ", this.state)
           );
         })
-        // .then(weather =>  this.setState({weather: weather}))
         .catch(error => console.log(error))
-        // .then(weather => weatherObj = {...weather})
-
-    // }
-
-    // console.log("This.state.city: ", this.state);
   }
-
-
-
-
+  
   render() {
 
       return (
@@ -96,10 +83,6 @@ handleSubmit = (e) => {
             zip={this.state.zipCode}
             submit={this.handleSubmit}/>
           <WeatherDisplay weather={this.state.weather} />
-
-
-
-
         </div>
           <Scroll>
             <CardList items={this.state.data} weather={this.state.weather}/>
@@ -112,28 +95,3 @@ handleSubmit = (e) => {
 }
 
 export default App;
-
-// <WeatherDisplay cityName={this.state.weather.city}
-
-// temp={this.state.weather.main.temp }
-// rain={this.state.weather.list.weather[0].description}/>
-
-// handleInput = (e) => {
-//   this.setState({weather: e.target.value})
-// }
-
-// <Searchfield searchChange={this.handleInput}/>
-
-  // <WeatherInput handleChange={this.handleInput} />
-
-  // <WeatherInput handleSubmit={this.weatherChange}  handleChange={this.handleChange}/>
-
-  // <Searchfield searchChange={this.handleInput}/>
-
-  // handleInput = (e) => {
-  //  let input = e.target.value
-  //  // console.log(input)
-  //    this.setState({ weather: input})
-  //  }
-
-  // weatherChange={this.weatherChange}
